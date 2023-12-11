09b2394800948e25cb5b2d467b7f53ab08dd8c3e
Dylan Schultz <9121234+dylanschultzie@users.noreply.github.com>
2023-12-11 15:11:15 -0800

    Fix Peggo naming
    Signed-off-by: Dylan Schultz <9121234+dylanschultzie@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -10,25 +10,25 @@ ORCHESTRATOR_ADDR = os.getenv('ORCHESTRATOR_ADDRESS')
 POLL_SECONDS = int(os.getenv('POLL_SECONDS'))
 HTTP_PORT = int(os.getenv('HTTP_PORT'))
 
-# Define a Gauge metric to track peggy event lag
-PEGGY_EVENT_LAG = Gauge('peggy_event_lag', 'Peggy event lag')
-PEGGY_NETWORK_NONCE = Gauge('peggy_network_nonce', 'Injective network current Peggy nonce')
-PEGGY_ORCHESTRATOR_NONCE = Gauge('peggy_orchestrator_nonce', 'Peggy orchestrator nonce')
+# Define a Gauge metric to track peggo event lag
+peggo_EVENT_LAG = Gauge('peggo_event_lag', 'Peggo event lag')
+peggo_NETWORK_NONCE = Gauge('peggo_network_nonce', 'Injective network current peggo nonce')
+peggo_ORCHESTRATOR_NONCE = Gauge('peggo_orchestrator_nonce', 'Peggo orchestrator nonce')
 
 def process_request():
     node_addr = NODE_URL
 
-    r = requests.get(f'{node_addr}/peggy/v1/module_state')
+    r = requests.get(f'{node_addr}/peggo/v1/module_state')
     network_nonce = int(r.json()['state']['last_observed_nonce'])
 
-    r = requests.get(f'{node_addr}/peggy/v1/oracle/event/{ORCHESTRATOR_ADDR}')
+    r = requests.get(f'{node_addr}/peggo/v1/oracle/event/{ORCHESTRATOR_ADDR}')
     orchestrator_nonce = int(r.json()['last_claim_event']['ethereum_event_nonce'])
 
     event_lag = network_nonce - orchestrator_nonce
 
-    PEGGY_EVENT_LAG.set(event_lag)
-    PEGGY_NETWORK_NONCE.set(network_nonce)
-    PEGGY_ORCHESTRATOR_NONCE.set(orchestrator_nonce)
+    peggo_EVENT_LAG.set(event_lag)
+    peggo_NETWORK_NONCE.set(network_nonce)
+    peggo_ORCHESTRATOR_NONCE.set(orchestrator_nonce)
 
 def main():
     start_http_server(HTTP_PORT)
